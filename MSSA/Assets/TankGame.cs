427446fca7387ab5930230f1cc96bf1643f857c6
Justin A Hughes <justin.a.hughes24@outlook.com>
2019-07-28 02:06:21 -0400

    Reinitialized the project in Unity. Started work on main menu

---
+++ MSSA/Assets/TankGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;// SerializeField creates an editable item for this variable in the Unity Inspector
+    [SerializeField] ScriptObjectList startingState;
+
+
+    ScriptObjectList currentState;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        currentState = startingState;
+        textComponent.text = currentState.ShowMenu();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+}
