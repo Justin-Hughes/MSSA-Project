b3a5f23e95fea106ee691226dd67532a34a61694
Justin Hughes <justin.a.hughes24@outlook.com>
2019-09-01 19:07:30 -0400

    Rearranged files to match assignment criteria and created and performed initial migration for project database

---
+++ MSSA/Model/Migrations/20190831230500_InitialMigrations.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MSSA.Model.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Statistics",
+                columns: table => new
+                {
+                    Username = table.Column<string>(nullable: false),
+                    Wins = table.Column<long>(nullable: false),
+                    Losses = table.Column<long>(nullable: false),
+                    Draws = table.Column<long>(nullable: false),
+                    TotalShotsFired = table.Column<long>(nullable: false),
+                    TotalDistanceTraveled = table.Column<long>(nullable: false),
+                    TotalDamageDealt = table.Column<long>(nullable: false),
+                    Accuracy = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistics", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tank",
+                columns: table => new
+                {
+                    Username = table.Column<string>(nullable: false),
+                    Health = table.Column<int>(nullable: false),
+                    WeaponName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tank", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weapon",
+                columns: table => new
+                {
+                    WeaponName = table.Column<string>(nullable: false),
+                    TrajectoryType = table.Column<string>(nullable: true),
+                    Damage = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weapon", x => x.WeaponName);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Statistics");
+
+            migrationBuilder.DropTable(
+                name: "Tank");
+
+            migrationBuilder.DropTable(
+                name: "Weapon");
+        }
+    }
+}
---
+++ MSSA/Model/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.ComponentModel.DataAnnotations;
+
+namespace MSSA.Model
+{
+    public class GDatabaseContext : DbContext
+    {
+        public DbSet<Tank> Tank { get; set; }
+        public DbSet<Weapon> Weapon { get; set; }
+        public DbSet<Statistics> Statistics { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;Database=Spanks");
+        }
+    }
+
+    public class Tank
+    {
+        [Key]
+        public string Username { get; set; }
+        public int Health { get; set; }
+        public string WeaponName { get; set; }
+    }
+    public class Weapon
+    {
+        [Key]
+        public string WeaponName { get; set; }
+        public string TrajectoryType { get; set; }
+        public int Damage { get; set; }
+    }
+
+    public class Statistics
+    {
+        [Key]
+        public string Username { get; set; }
+        public uint Wins { get; set; }
+        public uint Losses { get; set; }
+        public uint Draws { get; set; }
+        public uint TotalShotsFired { get; set; }
+        public uint TotalDistanceTraveled { get; set; }
+        public uint TotalDamageDealt { get; set; }
+        public double Accuracy { get; set; }
+    }
+}
