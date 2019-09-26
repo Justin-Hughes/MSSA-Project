d26966a95ed5598addba13f258b1343b9d562f44
Justin-Hughes <justin.a.hughes24@outlook.com>
2019-09-25 20:45:52 -0400

    Deleted all scripts and started fresh on code side. Removed backup files from audio folder. Deleted scene transition animations.

---
+++ MSSA/Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+    void Update()
+    {
+
+    }
+
+    IEnumerator LoadScene() // Coroutine so that your transition animation plays through all the way
+    {
+        transitionAnim.SetTrigger("end");//Begins the selected transition
+        yield return new WaitForSeconds(1.5f);// = Thread.Sleep() (in seconds)
+        SceneManager.LoadScene(sceneName);// Changes the scene
+    }
+}
