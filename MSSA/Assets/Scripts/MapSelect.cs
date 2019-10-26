328148291e1513f15c4963d2f39915fd92540890
Justin Hughes <justin.a.hughes24@outlook.com>
2019-10-26 17:28:18 -0400

    Added Sprites.

--- MSSA/Assets/Scripts/MapSelect.cs
+++ MSSA/Assets/Scripts/MapSelect.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(int level)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(level);
     }
 }
